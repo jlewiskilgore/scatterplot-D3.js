a9d8cf3574d57d6f62daacdf5dd56f441b81174f
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-04-06 20:20:46 -0400

    Append svg to scatterplot element

--- js/ScatterplotGraph.js
+++ js/ScatterplotGraph.js
@@ -3,4 +3,28 @@ var dataUrl = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceDa
 d3.json(dataUrl, function(json) {
 	console.log(json);
 	var dataSet = json;
+
+	var height = 550;
+	var width = 650;
+
+	d3.select(".scatterplot-title").text("Scatterplot of Doping in Professional Cycling");
+
+	var svg = d3.select(".scatterplot-graph")
+		.attr("width", width)
+		.attr("height", height)
+		.attr("viewBox", "1200 -100 50 400");
+
+	svg.selectAll("circle")
+		.data(dataSet)
+		.enter()
+		.append("circle")
+		.attr("cx", function(d) {
+			console.log(d.Seconds);
+			return d.Seconds / 2;
+		})
+		.attr("cy", function(d) {
+			console.log(d.Place);
+			return d.Place * 5;
+		})
+		.attr("r", 5);
 });
