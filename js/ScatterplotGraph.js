aad62852418556dfaede8e02ae1db911fdbb30a0
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-04-07 21:46:09 -0400

    Add X and Y axis scales

--- js/ScatterplotGraph.js
+++ js/ScatterplotGraph.js
@@ -4,27 +4,41 @@ d3.json(dataUrl, function(json) {
 	console.log(json);
 	var dataSet = json;
 
-	var height = 550;
-	var width = 650;
+	var height = 600;
+	var width = 600;
 
 	d3.select(".scatterplot-title").text("Scatterplot of Doping in Professional Cycling");
 
 	var svg = d3.select(".scatterplot-graph")
 		.attr("width", width)
 		.attr("height", height)
-		.attr("viewBox", "1200 -100 50 400");
+		.attr("viewBox", "100 -25 400 400");
+
+	var xScale = d3.scaleLinear()
+		.domain([2000, 2500])
+		.range([0, width]);
+
+	var xAxis = d3.axisBottom()
+		.scale(xScale)
+		.ticks(10);
+
+	var yScale = d3.scaleLinear()
+		.domain([1, 40])
+		.range([0, height]);
+
+	var yAxis = d3.axisLeft()
+		.scale(yScale)
+		.ticks(10);
 
 	svg.selectAll("circle")
 		.data(dataSet)
 		.enter()
 		.append("circle")
 		.attr("cx", function(d) {
-			console.log(d.Seconds);
-			return d.Seconds / 2;
+			return d.Seconds - 2000;
 		})
 		.attr("cy", function(d) {
-			console.log(d.Place);
-			return d.Place * 5;
+			return d.Place * 10;
 		})
 		.attr("r", 5);
 });
